053e2304afc1d2b16c322307831eafa33036bd25
neustroev.ruslan <neustroev.ruslan@wb.ru>
2026-06-04 12:32:39 +0900

    draft

--- deploy/nginx/init-letsencrypt.sh
+++ deploy/nginx/init-letsencrypt.sh
@@ -2,15 +2,14 @@ echo "### Запрашиваем настоящий сертификат у Let'
 staging_arg=""
 if [ $STAGING != 0 ]; then staging_arg="--staging"; fi
 
-# МЕНЯЕМ "run --rm --entrypoint" НА "exec"
-docker compose exec certbot certbot certonly --webroot -w /var/www/certbot \
-    $staging_arg \
-    -d $DOMAIN \
-    --email $EMAIL \
-    --rsa-key-size 4096 \
-    --agree-tos \
-    --force-renewal \
-    --non-interactive
+docker compose run \
+  --rm certbot certonly \
+  --webroot \
+  --webroot-path=/var/www/certbot \
+  -d $DOMAIN \
+  --email $EMAIL \
+  --agree-tos \
+  --no-eff-email
 
 echo "### Перезапускаем Nginx для применения сертификатов..."
 docker compose exec nginx nginx -s reload
